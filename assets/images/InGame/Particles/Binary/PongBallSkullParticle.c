314d6c9fde898122d75fc6f08d89fbed3247c203
Jorge Eremiev <jorgeche@Jorges-MacBook-Pro-2.local>
2019-01-09 15:24:17 -0500

    Cleaned up first empty char produced by grit.

--- assets/images/InGame/Particles/Binary/PongBallSkullParticle.c
+++ assets/images/InGame/Particles/Binary/PongBallSkullParticle.c
@@ -1,10 +1,11 @@
 
+
 //{{BLOCK(PongBallSkullParticle)
 
 //======================================================================
 //
 //	PongBallSkullParticle, 8x32@2, 
-//	+ 5 tiles not compressed
+//	+ 4 tiles not compressed
 //	+ regular map (flat), not compressed, 1x4 
 //	Total size: 80 + 8 = 88
 //
@@ -15,14 +16,14 @@
 
 const unsigned int PongBallSkullParticleTiles[20] __attribute__((aligned(4)))=
 {
-	0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x3CCF0FFC,0x3FFF3CCF,0x03300FFC,
+	0x00000000,0x3CCF0FFC,0x3FFF3CCF,0x03300FFC,
 	0x00000000,0x288A0AA8,0x2AAA288A,0x02200AA8,0x00000000,0x14450554,0x15551445,0x01100554,
 	0x00000000,0x00000000,0x00000000,0x00000000,
 };
 
 const unsigned short PongBallSkullParticleMap[4] __attribute__((aligned(4)))=
 {
-	0x0001,0x0002,0x0003,0x0004,
+	0x0000,0x0001,0x0002,0x0003,
 };
 
 //}}BLOCK(PongBallSkullParticle)
