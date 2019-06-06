8e9037336d34d55148c5ca7e719aea16b4ad3708
Christian Radke <chris@vr32.de>
2019-06-06 21:17:46 +0200

    Upadated project files

--- assets/languages/Language_ES.c
+++ assets/languages/Language_ES.c
@@ -1,23 +1,6 @@
-/* VUEngine - Virtual Utopia Engine <http://vuengine.planetvb.com/>
- * A universal game engine for the Nintendo Virtual Boy
- *
- * Copyright (C) 2007, 2018 by Jorge Eremiev <jorgech3@gmail.com> and Christian Radke <chris@vr32.de>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
- * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
- * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                              THIS FILE WAS AUTO-GENERATED - DO NOT EDIT                               //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 //---------------------------------------------------------------------------------------------------------
@@ -41,11 +24,14 @@ extern EntitySpec FLAG_SPAIN_EN;
 /*
  * IMPORTANT: Ensure that this file is encoded in Windows-1252 or ISO-8859-1 ("ANSI") to make use
  * of the full extended ASCII character set including special characters of European languages.
+ *
+ * The following character is meant to prevent editors from loading this file as UTF-8, since it doesn't
+ * form part of a valid UTF-8 byte sequence: ý
  */
 
 const char* const LANGUAGE_ES_STRINGS[] =
 {
-	""
+	
 };
 
 const LangROMSpec LANGUAGE_ES =
@@ -55,7 +41,7 @@ const LangROMSpec LANGUAGE_ES =
 
 	// Flag Entity
 	&FLAG_SPAIN_EN,
-
+	
 	// Strings
 	(const char**)LANGUAGE_ES_STRINGS
 };
