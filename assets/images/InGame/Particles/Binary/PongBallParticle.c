f9168c393d9b82771eb27278d955592d15059eb8
Christian Radke <christian.radke@unicepta.com>
2019-06-07 11:38:59 +0200

    Some adjustments

--- assets/images/InGame/Particles/Binary/PongBallParticle.c
+++ assets/images/InGame/Particles/Binary/PongBallParticle.c
@@ -1,10 +1,11 @@
 
+
 //{{BLOCK(PongBallParticle)
 
 //======================================================================
 //
 //	PongBallParticle, 8x40@2, 
-//	+ 6 tiles not compressed
+//	+ 5 tiles not compressed
 //	+ regular map (flat), not compressed, 1x5 
 //	Total size: 96 + 12 = 108
 //
@@ -15,14 +16,14 @@
 
 const unsigned int PongBallParticleTiles[24] __attribute__((aligned(4)))=
 {
-	0x00000000,0x00000000,0x00000000,0x00000000,0x00C00000,0x3FF003C0,0x03C00FFC,0x00000300,
+	0x00C00000,0x3FF003C0,0x03C00FFC,0x00000300,
 	0x00000000,0x03F00300,0x00C00FC0,0x00000000,0x00000000,0x03C00000,0x000003C0,0x00000000,
 	0x00000000,0x00C00000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,
 };
 
 const unsigned short PongBallParticleMap[6] __attribute__((aligned(4)))=
 {
-	0x0001,0x0002,0x0003,0x0004,0x0005,0x0000,
+	0x0000,0x0001,0x0002,0x0003,0x0004,0x0000,
 };
 
 //}}BLOCK(PongBallParticle)
